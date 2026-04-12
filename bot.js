eaeb1b12617f5c52ad628cef3c3c30f00de9c8f7
sono-ro-hub <nolfodshrosario@gmail.com>
2026-04-12 23:01:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const fs = require("fs");
-const config = require("./config.json");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
@@ -29,4 +28,4 @@ client.on("interactionCreate", async interaction => {
   await command.execute(interaction, client);
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);
