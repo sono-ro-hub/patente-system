9fed91612f2eaa799b2df71afb821fadf9ab092a
sono-ro-hub <nolfodshrosario@gmail.com>
2026-04-14 18:28:41 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = require("./config.json");
+
+module.exports = {
+  CANALE_RICHIESTE: "1493595963942768860",
+  CANALE_STAFF: "1493597555760824503",
+
+  GUILD_ID: config.guildId,
+  STAFF_ROLE: config.staffRoleId,
+  PATENTE_ROLE: config.patenteRoleId
+};
